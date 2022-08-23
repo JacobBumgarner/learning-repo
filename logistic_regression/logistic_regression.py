30e439b1871c45e8e1259ebaecd375ece21d3bf6
Leonardo Dalla Porta <leonardodallaporta@gmail.com>
2022-08-23 16:45:55 +0200

    Update logistic_regression.py
    Just for consistency

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -187,7 +187,7 @@ class LogisticRegression:
         self.fit = True
         return np.array(costs), np.array(accuracies), np.array(weight_hist).T, np.array(bias_hist).T[0, 0]
 
-    def predict(self, X) -> np.ndarray:
+    def predict(self, X: np.ndarray) -> np.ndarray:
         """Predict the labels for a set of input data.
 
         Args:
